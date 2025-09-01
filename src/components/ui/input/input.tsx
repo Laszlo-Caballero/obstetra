f7841a047ebd90f5926cf0184d5d744a34f21787
laszlo-caballero <laszlocaballero@gmail.com>
2025-08-31 21:05:51 -0500

    Refactor Input component to use a more structured className prop and improve type definitions

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import cx from '@/libs/cx';
+import React, { InputHTMLAttributes } from "react";
+import cx from "@/libs/cx";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    label: string;
-    id:string;
-    className?: string;
-    classNameLabel?: string;
+interface InputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "className"> {
+  label: string;
+  id: string;
+  className?: {
+    input?: string;
+    label?: string;
+    container?: string;
+  };
 }
-export default function Input({label, className, id, classNameLabel, ...props}: InputProps) {
-    return (
-        <div className="flex flex-col gap-y-1">
-            <label className={cx("text-ob-gray-2 font-medium", classNameLabel)} htmlFor={id}>
-                {label}
-            </label>
-            <input
-            className={cx("bg-ob-black-4 rounded-xl font-medium text-sm placeholder-ob-white py-2 px-3 border border-ob-gray",className)}
-            id={id}
-            {...props}
-            />
-        </div>
-    )
+export default function Input({ label, className, id, ...props }: InputProps) {
+  return (
+    <div className={cx("flex flex-col gap-y-1", className?.container)}>
+      <label
+        className={cx("text-ob-gray-2 font-medium", className?.label)}
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <input
+        className={cx(
+          "bg-ob-black-4 rounded-xl font-medium text-sm placeholder-ob-white py-2 px-3 border border-ob-gray",
+          className?.input
+        )}
+        id={id}
+        {...props}
+      />
+    </div>
+  );
 }
-
