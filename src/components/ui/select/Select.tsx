9a7c176658cf9d120b01f9d00ebe632a8f15deee
luciana-31 <lgogny74@gmail.com>
2025-08-31 22:09:29 -0500

    Refactor Select component to use a structured className prop for improved styling flexibility

--- src/components/ui/select/Select.tsx
+++ src/components/ui/select/Select.tsx
@@ -15,7 +15,13 @@ interface SelectProps {
   placeholder?: string;
   search?: string;
   options?: Options[];
-  className?: string;
+  className?:{
+    label?: string;
+    placeholder?: string;
+    optionsContainer?: string;
+    search?: string;
+    optionsItem?: string;
+  }
   icon?: ReactNode;
 }
 
@@ -31,20 +37,20 @@ export default function Select({
   const ref = useClose({closeFunction:setOpen});
 
   return <div className="flex flex-col gap-y-1 relative" ref={ref}>
-    <label className="text-ob-gray-2 font-medium">
+    <label className={cx("text-ob-gray-2 font-medium", className?.label)}>
       {label}
     </label>
-    <div className="flex items-center justify-between bg-ob-black-4 rounded-xl font-medium text-sm py-2 px-3 border border-ob-gray cursor-pointer" onClick={()=>setOpen(!isOpen)}>
-      <p className="text-ob-white">
+    <div className={cx("flex items-center justify-between text-ob-white bg-ob-black-4 rounded-xl font-medium text-sm py-2 px-3 border border-ob-gray cursor-pointer",className?.placeholder)} onClick={()=>setOpen(!isOpen)}>
+      <span>
         {placeholder}
-      </p>
+      </span>
       <span>
         <RiArrowDropDownLine className="text-ob-white" size={18} />
       </span>
     </div>
     {isOpen && 
-        <div className="absolute traslate-y-2 mt-1 top-full bg-ob-black-6 border border-ob-gray rounded-3xl p-3 w-full ">
-          <div className="flex items-center gap-x-2 bg-ob-black-4 border border-ob-gray rounded-xl p-2 mb-2">
+        <div className={cx("absolute traslate-y-2 mt-1 top-full bg-ob-black-6 border border-ob-gray rounded-3xl p-3 w-full ",className?.optionsContainer)}>
+          <div className={cx("flex items-center gap-x-2 bg-ob-black-4 border border-ob-gray rounded-xl p-2 mb-2", className?.search)}>
             <span>
               <CiSearch className="text-ob-white" size={18} />
             </span>
@@ -53,8 +59,7 @@ export default function Select({
           <div className="max-h-[100px] overflow-y-scroll">
           {options?.map((option)=>{
             return (
-              
-            <div className="flex items-center gap-x-2 p-3 hover:bg-ob-black-4 rounded-xl cursor-pointer" onClick={()=>setOpen(false)}>
+            <div className={cx("flex items-center gap-x-2 p-3 hover:bg-ob-black-4 rounded-xl cursor-pointer",className?.optionsItem)} onClick={()=>setOpen(false)}>
               <span>
                 {icon}
               </span>
