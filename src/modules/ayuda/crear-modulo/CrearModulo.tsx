7e7e8c344413028413733e03369e99b71e49f39d
luciana-31 <lgogny74@gmail.com>
2025-09-04 19:50:59 -0500

    Refactor modal components in CrearModulo and CrearTipo; update to use new modal-v2 structure and improve button functionality.

--- src/modules/ayuda/crear-modulo/CrearModulo.tsx
+++ src/modules/ayuda/crear-modulo/CrearModulo.tsx
@@ -1,20 +1,28 @@
 import React from "react";
-import Modal from "@/components/ui/modal/Modal";
+import Modal from "@/components/ui/modal-v2/Modal";
 import { HiOutlineSquares2X2 } from "react-icons/hi2";
-import { LuSave, LuTag } from "react-icons/lu";
+import { LuSave, LuTag, LuX } from "react-icons/lu";
 import Input from "@/components/ui/input/input";
 import TextArea from "@/components/ui/textarea/Textarea";
+import ModalHeader from "@/components/ui/modal-v2/modal-header/ModalHeader";
+import ModalTitle from "@/components/ui/modal-v2/modal-title/ModalTitle";
+import CloseButton from "@/components/ui/modal-v2/close-button/CloseButton";
+import ModalFooter from "@/components/ui/modal-v2/modal-footer/ModalFooter";
+import ContainerButton from "@/components/ui/modal-v2/container-button/ContainerButton";
+import Button from "@/components/ui/button/Button";
 
 export default function CrearModulo() {
   return (
-    <Modal
-      title="Crear Modulo"
-      badge="Basico"
-      button="Guardar"
-      nota="Se le notificara a todos los administradores"
-      iconButton={<LuSave size={18} />}
-      icon={<HiOutlineSquares2X2 size={20} />}
-    >
+    <Modal>
+      <ModalHeader>
+        <ModalTitle title="Crear Modulo" badge="Basico">
+          <HiOutlineSquares2X2 size={20} />
+        </ModalTitle>
+        <CloseButton>
+          <LuX size={18} className="text-ob-white" />
+        </CloseButton>
+      </ModalHeader>
+
       <div className="flex flex-col gap-y-3 p-4">
         <span className="text-ob-gray-2 text-sm">
           Agrega un Módulo Basico. Solo necesitas dos datos
@@ -44,6 +52,16 @@ export default function CrearModulo() {
           </span>
         </div>
       </div>
+
+      <ModalFooter nota="Se le notificara a todos los administradores">
+        <ContainerButton>
+          <CloseButton>Cancelar</CloseButton>
+          <Button className="font-semibold bg-ob-teal">
+            <LuSave size={18} />
+            Guardar
+          </Button>
+        </ContainerButton>
+      </ModalFooter>
     </Modal>
   );
 }
