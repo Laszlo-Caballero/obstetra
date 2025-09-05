07aed541f6eb9bb08fe7cc88d8c4e399ca6794b6
laszlo-caballero <laszlocaballero@gmail.com>
2025-09-04 19:51:30 -0500

    Refactor ModalImport to use the updated modal-v2 components; enhance structure and maintainability.

--- src/modules/posta/import/ModalImport.tsx
+++ src/modules/posta/import/ModalImport.tsx
@@ -1,6 +1,23 @@
-import Modal from "@/components/ui/modal/Modal";
+import Modal from "@/components/ui/modal-v2/Modal";
+import ModalHeader from "@/components/ui/modal-v2/modal-header/ModalHeader";
+import ModalTitle from "@/components/ui/modal-v2/modal-title/ModalTitle";
 import React from "react";
+import { LuUpload } from "react-icons/lu";
 
-export default function ModalImport() {
-  return <Modal title="Subir archivo (solo Excel)"></Modal>;
+interface ModalImportProps {
+  onClose?: () => void;
+}
+
+export default function ModalImport({ onClose }: ModalImportProps) {
+  return (
+    <Modal onClose={onClose}>
+      <ModalHeader>
+        <ModalTitle title="Subir archivo (solo Excel)">
+          <span className="block size-2 bg-ob-blue-3">
+            <LuUpload />
+          </span>
+        </ModalTitle>
+      </ModalHeader>
+    </Modal>
+  );
 }
