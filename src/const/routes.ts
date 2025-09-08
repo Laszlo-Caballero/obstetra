e076959772f5e160863e805d01cfe1d0fdcbda54
laszlo-caballero <laszlocaballero@gmail.com>
2025-09-07 20:14:48 -0500

    Añadir rutas vacías para obstetras y enfermeras en el archivo de rutas

--- src/const/routes.ts
+++ src/const/routes.ts
@@ -1,2 +1,4 @@
 export const admin_paths = ["/ayudar/administrar"];
+export const obstetras_paths = [""];
+export const enfermeras_paths = [];
 export const public_paths = ["/login", "/public/ayuda"];
