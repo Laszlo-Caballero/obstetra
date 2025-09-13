c6f9873635c1151d517e89cfd36204875b88a96b
laszlo-caballero <laszlocaballero@gmail.com>
2025-09-13 18:32:58 -0500

    feat: reestructurar páginas de administración de medicina y atención; agregar nuevos elementos al menú lateral

--- src/const/asideData.tsx
+++ src/const/asideData.tsx
@@ -1,7 +1,10 @@
 import { AsideProps } from "@/interface/aside";
 import { SiDatabricks } from "react-icons/si";
-import { LuCalendar, LuStethoscope } from "react-icons/lu";
+import { LuCalendar, LuPersonStanding, LuStethoscope } from "react-icons/lu";
 import { LuBuilding2 } from "react-icons/lu";
+import { MdOutlineQueuePlayNext } from "react-icons/md";
+import { GiMedicines } from "react-icons/gi";
+import { IoMdPhotos } from "react-icons/io";
 export const asideData: AsideProps[] = [
   {
     icon: <SiDatabricks />,
@@ -22,5 +25,25 @@ export const asideData: AsideProps[] = [
     icon: <LuCalendar />,
     title: "Citas",
     href: "/citas",
+  },
+  {
+    icon: <LuPersonStanding />,
+    title: "Pacientes",
+    href: "/paciente",
+  },
+  {
+    icon: <MdOutlineQueuePlayNext />,
+    title: "Atenciones",
+    href: "/atencion",
+  },
+  {
+    icon: <GiMedicines />,
+    title: "Medicina",
+    href: "/medicina",
+  },
+  {
+    icon: <IoMdPhotos />,
+    title: "Galeria",
+    href: "/galeria",
   },
 ];
