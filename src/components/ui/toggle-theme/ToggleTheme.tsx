8c16472ac806e8a4246bc5b12f174c5f301dd689
laszlocaballero <laszlocaballero@gmail.com>
2025-10-21 22:26:17 -0500

    feat: agregar componente ToggleTheme en el encabezado y mejorar estilos de iconos

--- src/components/ui/toggle-theme/ToggleTheme.tsx
+++ src/components/ui/toggle-theme/ToggleTheme.tsx
@@ -17,7 +17,7 @@ export default function ToggleTheme() {
         }
       }}
     >
-      {theme === 'light' ? <LuMoon size={20} /> : <LuSun size={20} />}
+      {theme === 'light' ? <LuMoon size={20} className="text-white" /> : <LuSun size={20} />}
     </button>
   );
 }
