9a941009c8022092dbf8bd5483085e2fb49db0fb
laszlocaballero <laszlocaballero@gmail.com>
2025-10-29 11:54:55 -0500

    Refactor authentication middleware: remove old middleware and implement proxy function for authentication handling
    - Deleted the existing middleware.ts file.
    - Created a new proxy.ts file to handle authentication logic.
    - Updated TypeScript configuration to improve JSX handling and include additional type definitions.

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    formats: ["image/webp"],
+    formats: ['image/webp'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/**",
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '3000',
+        pathname: '/**',
       },
     ],
   },
+  reactCompiler: true,
 };
 
 export default nextConfig;
